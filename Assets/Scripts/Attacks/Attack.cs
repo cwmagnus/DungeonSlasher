efa4b9ac1913678e91e5ba6e33617d01da8f3be6
cwmagnus <cwmagnus0@gmail.com>
2018-06-19 22:11:27 -0400

    Created attack base class that can be used for both the player and the enemy

---
+++ Assets/Scripts/Attacks/Attack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace DungeonSlasher
+{
+    /// <summary>
+    /// Base attack class for all attacks in the game.
+    /// </summary>
+    public abstract class Attack : MonoBehaviour
+    {
+        [Header("Attack Properties")]
+        [SerializeField, Tooltip("Amount of damage the attack will do.")] 
+        protected float damage;
+        [SerializeField, Tooltip("Attack range.")] 
+        protected float range;
+        [SerializeField, Tooltip("How long the attack lasts.")]
+        protected float duration;
+        [SerializeField, Tooltip("How often the attack can be used.")] 
+        protected float cooldown;
+        protected float maxCooldown;
+        [SerializeField, Tooltip("Name of the attack animation to play.")]
+        protected string animationName;
+        protected bool attacking;
+
+        /// <summary>
+        /// Set default variables.
+        /// </summary>
+        private void Awake()
+        {
+            maxCooldown = cooldown;
+            OnAwake();
+        }
+
+        /// <summary>
+        /// Gets called on awake, use this to set up the attack.
+        /// </summary>
+        protected virtual void OnAwake() { }
+
+        /// <summary>
+        /// Use the attack.
+        /// </summary>
+        protected abstract void UseAttack();
+    }
+}
---
+++ Assets/Scripts/Attacks/PlayerAttack.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DungeonSlasher
+{
+    /// <summary>
+    /// Handles attack button clicks for the player.
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    public abstract class PlayerAttack : Attack
+    {
+        [SerializeField, Tooltip("Player tag to look for.")]
+        protected string playerTag;
+        protected Button attackButton;
+        protected GameObject player;
+        protected bool foundPlayer;
+
+        /// <summary>
+        /// Bind the attack function to the attack button.
+        /// </summary>
+        protected override void OnAwake()
+        {
+            attackButton = GetComponent<Button>();
+            attackButton.onClick.AddListener(UseAttack);
+        }
+
+        /// <summary>
+        /// Update the attack every frame.
+        /// </summary>
+        private void Update()
+        {
+            if (foundPlayer)
+            {
+                OnUpdate();
+
+                if (attacking) OnAttacking();
+
+                cooldown -= Time.deltaTime;
+            }
+            else FindPlayer(playerTag);
+        }
+
+        /// <summary>
+        /// Finds the player.
+        /// </summary>
+        /// <param name="playerTag">Player tag to find.</param>
+        protected void FindPlayer(string playerTag)
+        {
+            player = GameObject.FindGameObjectWithTag(playerTag);
+            foundPlayer = player != null;
+        }
+
+        /// <summary>
+        /// Use the attack.
+        /// </summary>
+        protected override void UseAttack()
+        {
+            if (foundPlayer)
+            {
+                if (cooldown <= 0) Attack();
+            }
+        }
+
+        /// <summary>
+        /// Called when the player attacks.
+        /// </summary>
+        protected abstract void Attack();
+
+        /// <summary>
+        /// Called after the player is found every frame.
+        /// </summary>
+        protected virtual void OnUpdate() { }
+
+        /// <summary>
+        /// Called every frame when attacking.
+        /// </summary>
+        protected virtual void OnAttacking() { }
+    }
+}
